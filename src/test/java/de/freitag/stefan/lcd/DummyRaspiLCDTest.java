46f2c0ef057ff2bd7002a5eed8dbdb195b795d35
Stefan Freitag <stefan.freitag@udo.edu>
2015-08-02 21:12:01 +0200

    Added unit tests

--- src/test/java/de/freitag/stefan/lcd/DummyRaspiLCDTest.java
+++ src/test/java/de/freitag/stefan/lcd/DummyRaspiLCDTest.java
@@ -52,4 +52,58 @@ public final class DummyRaspiLCDTest {
         final DummyRaspiLCD lcd = new DummyRaspiLCD();
         lcd.drawLine(new Point(1, 1), null);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setContrastWithValueOutOfBoundsLeftThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.setContrast(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setContrastWithValueOutOfBoundsRightThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.setContrast(64);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putCharacterWithNullPointThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.putCharacter(null, new Character('s'));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putCharacterWithNullCharacterThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.putCharacter(new Point(0, 0), null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putStringWithNullPointThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.putString(null, "Text");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putStringWithNullStringThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.putString(new Point(0, 0), null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putPixelWithNullPointThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.putPixel(null, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void drawRectangleWithNullUpperLeftThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.drawRectangle(null, new Point(1, 1), 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void drawRectangleWithNullLowerRightThrowsIllegalArgumentException() {
+        final DummyRaspiLCD lcd = new DummyRaspiLCD();
+        lcd.drawRectangle(new Point(1, 1), null, 1);
+    }
 }
--- src/test/java/de/freitag/stefan/lcd/Pi4JRaspiLCDTest.java
+++ src/test/java/de/freitag/stefan/lcd/Pi4JRaspiLCDTest.java
@@ -52,4 +52,58 @@ public class Pi4JRaspiLCDTest {
         final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
         lcd.drawLine(new Point(1, 1), null);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setContrastWithValueOutOfBoundsLeftThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.setContrast(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setContrastWithValueOutOfBoundsRightThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.setContrast(64);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putCharacterWithNullPointThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.putCharacter(null, new Character('s'));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putCharacterWithNullCharacterThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.putCharacter(new Point(0, 0), null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putStringWithNullPointThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.putString(null, "Text");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putStringWithNullStringThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.putString(new Point(0, 0), null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putPixelWithNullPointThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.putPixel(null, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void drawRectangleWithNullUpperLeftThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.drawRectangle(null, new Point(1, 1), 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void drawRectangleWithNullLowerRightThrowsIllegalArgumentException() {
+        final Pi4JRaspiLCD lcd = new Pi4JRaspiLCD();
+        lcd.drawRectangle(new Point(1, 1), null, 1);
+    }
 }
